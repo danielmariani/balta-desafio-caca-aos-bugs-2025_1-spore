7c85c7f3623245304aece05f7255158f1737ad25
Daniel Mariani <eng.danielmariani@gmail.com>
2025-10-26 19:19:16 -0300

    Desafio finalizado. Aplicação rodando com Sqlite (app.db) após migrafion criada e aplicada no banco.

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,13 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var con = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(con));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
